35918939db508584e5115935ae14297a57713956
Arthi Palaniapan <arthipalaniapan@Arthis-MacBook-Pro.local>
2018-02-12 12:02:42 -0500

    first commit

---
+++ MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  Lib1
+//
+//  Created by Arthi Palaniapan on 2/11/18.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
